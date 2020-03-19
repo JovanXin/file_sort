07c6be828e87fa86d47a99eccbebeba97471f8e6
Jovan Xin <61258436+JovanXin@users.noreply.github.com>
2020-03-19 15:56:26 +1300

    Create filesort.py
    Project on hold - more solutions than answers

---
+++ filesort.py
@@ -0,0 +1,30 @@
+# with open("ex_list.txt", 'r') as file_1, open("sol_list.txt", "a") as file_2:
+#   for line in file_1:
+#     if ("Solution:") in line:
+#       file_2.write(line)
+#     else:
+#       print("not in line bro")
+
+cur_line = 0 
+starting_lines = []
+ending_lines = []
+final_lines = []
+i = 1
+
+
+with open("ex_list.txt", 'r') as file_1, open("sol_list.txt", "a") as file_2:
+  for line in file_1:
+    cur_line += 1
+    if ("Solution") in line:
+      starting_lines.append(str(cur_line))
+      file_2.write(line)
+    elif ("Question") in line:
+      ending_lines.append(str(cur_line))
+      file_2.write(line)
+
+
+print(", ".join(starting_lines)) 
+print(", ".join(ending_lines))
+
+print(f"There are {len(starting_lines)} Questions") #well, there seems to be more solutions than answers... so this project is a faliure
+print(f"There are {len(ending_lines)} Solutions") 
